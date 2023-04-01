7b3df35949cdc4fe8f44cf0987968293a8595591
Ealvarez01 <e.alvarez2404@yahoo.com>
2023-03-31 21:24:09 -0700

    started assessment working on challenges

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,7 @@
 // ASSESSMENT 3: Coding Practical Questions with Jest
 
+const { number } = require("yargs")
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -14,18 +16,106 @@
 
 // a) Create a test with expect statements for each of the variables provided.
 
+// Process: RED!
+// function takes in a number (greater than 2) and;
+describe("fib", () => { 
+  it("returns an array that length containing the numbers of the Fibonacci sequence.", () => {
+  // expect statement will contain the function call with a jest matcher appended to the end
+  // place test variables inside the it call back funtion
+    expect(fib(fibLength1)).toEqual([1, 1, 2, 3, 5, 8])
+    expect(fib(fibLength2)).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
+  })
+})
+// FAIL!: "fib is not defined"
+//  FAIL  ./code-challenges.test.js
+//   fib
+//   ✕ returns an array that length containing the numbers of the Fibonacci sequence. (1 ms)
+
+// ● fib › returns an array that length containing the numbers of the Fibonacci sequence.
+
+//   ReferenceError: fib is not defined
+
+// b) Create the function that makes the test pass.
+// Process: GREEN!
+
+// PSEUDOCODE:
+// Input:2 arrays
+// Output: 2 arrays with numbers greater than 2
+
+// Given array outputs (Fibonacci numbers):
+// Input: 6 numbers
+// Output: [1, 1, 2, 3, 5, 8]
+
+// Input: 10 numbers
+// Output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+
+// Create a function named "fib" that takes in a number (greater than 2)
+// Returns an array that length containing the numbers of the Fibonacci sequence (***sequence follows the rule that each number is equal to the sum of the next two numbers***).
+
 const fibLength1 = 6
 // Expected output: [1, 1, 2, 3, 5, 8]
 
 const fibLength2 = 10
 // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
-// b) Create the function that makes the test pass.
+// const fib = (number)
+//   let number =  []
+//     if(number.length.indexOf[0] + number.length.indexOf[1] === + 2) {
+//       return `${number} is equal to the sum of the next two numbers`
+//     }
+
+  // for(let i = 0; <fib.length; i++) {
+  //   if(fib[i] > 2===2) {
+  //     return `${fib[i]} + is equal to the next two numbers`
+  // } 
+  // else {fib[i] !== 2
+  //   return `${fibNum} is not equal to the sum of the next two numbers`
+  // }
+// }
 
 // --------------------2) Create a function that takes in an object and returns an array of the values sorted from least to greatest.
 // Hint: Check out this resource: Object.values() https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_objects/Object/values
 
 // a) Create a test with expect statements for each of the variables provided.
+
+// Process: RED!
+// function takes in an object and;
+describe("myObj", () => { 
+  it("returns an array of the values sorted from least to greatest.", () => {
+  // expect statement will contain the function call with a jest matcher appended to the end
+  // place test variables inside the it call back funtion
+    expect(myObj(studyMinutesWeek1 )).toEqual([15, 15, 20, 30, 30, 60, 90])
+    expect(myObj(studyMinutesWeek2)).toEqual([10, 15, 20, 45, 60, 65, 100])
+  })
+})
+// FAIL!: "obj is not defined" 
+// FAIL  ./code-challenges.test.js
+// obj
+//   ✕ returns an array of the values sorted from least to greatest.
+
+//     at Object.expect (code-challenges.test.js:23:5)
+
+// ● obj › returns an array of the values sorted from least to greatest.
+
+//   ReferenceError: obj is not defined
+
+// b) Create the function that makes the test pass.
+
+// Process: GREEN!
+
+// PSEUDOCODE:
+// Input:2 functions with property values
+// Output: an array with values sorted from least to greatest
+
+// Given outputs:
+// Input: sunday: 90, monday: 30, tuesday: 20, wednesday: 15, thursday: 30, friday: 15, saturday: 60
+// Output: [15, 15, 20, 30, 30, 60, 90]
+
+// Input: sunday: 100, monday: 10, tuesday: 45, wednesday: 60, thursday: 20, friday: 15, saturday: 65
+// Output: [10, 15, 20, 45, 60, 65, 100]
+
+// Create a function named "myObj" that takes in an object
+// Return an array of the values sorted from least to greatest
 
 const studyMinutesWeek1 = {
   sunday: 90,
@@ -49,11 +139,50 @@ const studyMinutesWeek2 = {
 }
 // Expected output: [10, 15, 20, 45, 60, 65, 100]
 
-// b) Create the function that makes the test pass.
+const myObj = {Object}
+
 
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
 // a) Create a test with expect statements for each of the variables provided.
+
+// Process: RED!
+// function takes in an array;
+describe(accounting, () => { 
+  it("returns an array of the accumulating sum. An empty array should return an empty array.", () => {
+  // expect statement will contain the function call with a jest matcher appended to the end
+  // place test variables inside the it call back funtion
+    expect(accounting(accountTransactions1)).toEqual([100, 83, 60, 51])
+    expect(accounting(accountTransactions2)).toEqual([250, 161, 261, 165])
+    expect(accounting(accountTransactions3)).toEqual([])
+  })
+})
+// // FAIL!: "accounting is not defined" 
+// FAIL  ./code-challenges.test.js
+//   ● Test suite failed to run
+
+//   ReferenceError: accounting is not defined
+
+// b) Create the function that makes the test pass.
+
+// Process: GREEN!
+
+// PSEUDOCODE:
+// Input:2 functions with property values
+// Output: an array of the accumulating sum. An empty array should return an empty array.
+
+// Given outputs:
+// Input: [100, -17, -23, -9]
+// Output: [100, 83, 60, 51]
+
+// Input: [250, -89, 100, -96]
+// Output: [250, 161, 261, 165]
+
+// Input: []
+// Output: []
+
+// Create a function named "accounting" takes in an array
+// Return an array of the accumulating sum. An empty array should return an empty array.
 
 const accountTransactions1 = [100, -17, -23, -9]
 // Expected output: [100, 83, 60, 51]
@@ -64,4 +193,8 @@ const accountTransactions2 = [250, -89, 100, -96]
 const accountTransactions3 = []
 // Expected output: []
 
-// b) Create the function that makes the test pass.
+const accounting = ([]) => {
+
+}
+
+
--- interview-questions.md
+++ interview-questions.md
@@ -9,36 +9,49 @@ Then, research the question to expand on your answer. Even if you feel you have 
 1. What is JSX? What is one syntax difference between HTML and JSX?
 
 Your answer:
+JSX is an extension in React that allows us to use html and have the functionality of JavaScript. It can be used to add additional elements in the return area. 
 
 Researched answer:
 
 1. What is yarn? What file(s) are modified when you run the command yarn in your terminal?
 
 Your answer:
+Yet
+Another
+Resource
+Negotiator
+Yarn is simply a code package manager. It provides the dependencies and snippets that are required to run your code.It modifies the active react file that was created in the Terminal.  
 
 Researched answer:
 
 3. What is a React component?
 
 Your answer:
+A react componnent is a seperate component file that is stored under the src (source) folder in a .js (JavaScript) file. When a component is created, it allows us to organize multiple functions that can be reused over and over again. You can all on components as many times as you need. Once your funtions are complete,  components can then be imported to one main App.js file that will hold all of the state variables, values and inputs and methods. 
 
 Researched answer:
 
 4. What is the difference between state values and props in React?
 
 Your answer:
+The difference between state values and props in react are that state values stores the data that determines how a component is displayed to the user. Props is short for properties in react and is just an object that has no value. 
 
 Researched answer:
 
 5. What is the DOM?
 
 Your answer:
+Document
+Object
+Model
+Organizes the html elements like a tree and gives a visual of those html elements, 
 
 Researched answer:
 
 6. STRETCH: Which is the difference between a div tag and a span tag?
 
 Your answer:
+A div tag divides or separates a certain section/block of code of your choice. A div tag can be used to target certain .css stylings you want applied only to one section of code by creating a className for your div tag. For a div tag, you would have to add a dot notation followed by the className. A span tag can also be used to sepearte a certain section of code but it requires an "id" className and can only be applied to a text funtion. Also, you would have to add a hashtag symbol(#) followed by the "id" name in .css in order for your styling to be applied to the specific text of your choce. 
 
 Researched answer:
 
