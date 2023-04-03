2e44b2f8dde5329a8dbdf88d290ddfabf0299924
Ealvarez01 <e.alvarez2404@yahoo.com>
2023-04-02 17:11:01 -0700

    react html complete still working on code challenges and interview questions

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,6 +1,6 @@
 // ASSESSMENT 3: Coding Practical Questions with Jest
 
-const { number } = require("yargs")
+// const { number } = require("yargs")
 
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
@@ -17,11 +17,12 @@ const { number } = require("yargs")
 // a) Create a test with expect statements for each of the variables provided.
 
 // Process: RED!
+
 // function takes in a number (greater than 2) and;
 describe("fib", () => { 
   it("returns an array that length containing the numbers of the Fibonacci sequence.", () => {
-  // expect statement will contain the function call with a jest matcher appended to the end
-  // place test variables inside the it call back funtion
+  // expect statement will contain the function call to call the given functions and;
+  // place test variable outputs inside the .toEqual() matcher parameter to compare the given outputs
     expect(fib(fibLength1)).toEqual([1, 1, 2, 3, 5, 8])
     expect(fib(fibLength2)).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
   })
@@ -36,21 +37,28 @@ describe("fib", () => {
 //   ReferenceError: fib is not defined
 
 // b) Create the function that makes the test pass.
+
 // Process: GREEN!
 
 // PSEUDOCODE:
 // Input:2 arrays
-// Output: 2 arrays with numbers greater than 2
+// Output: 2 arrays with each number is equal to the sum of the previous two numbers
 
 // Given array outputs (Fibonacci numbers):
-// Input: 6 numbers
+// Input: 6 numbers length
 // Output: [1, 1, 2, 3, 5, 8]
 
-// Input: 10 numbers
+// Input: 10 numbers length
 // Output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
-// Create a function named "fib" that takes in a number (greater than 2)
-// Returns an array that length containing the numbers of the Fibonacci sequence (***sequence follows the rule that each number is equal to the sum of the next two numbers***).
+// Create a function named "fib" that takes in a number in the parameter named "num". 
+// Create an array named "fibArray" that takes the 2 indexes of 1 for each given array output (since index starts at zero you need to take index one to start at the second number value for the fibonacci sequence). 
+// For the number values of the index to be greater than 2 and equal to the two previous numbers, create a for loop to let the index equal two index elements for each index number in the array.
+// Set an fibArray index element minus 1 (to get the last previous element in the array).
+// Set a fibArray index element minus 2 (to get the second to last previous element in the array).
+// Use the math logic for the sum of two index elements added together to equal the next index and therefore that index will be equal to the sum of the previous two elements.
+// Use the .slice() method to return each element value as a new array without changing the original array length.
+// Return array containing the numbers of the Fibonacci sequence (***sequence follows the rule that each number is equal to the sum of the next two numbers*** -----> https://en.wikipedia.org/wiki/Fibonacci_sequence <-----).
 
 const fibLength1 = 6
 // Expected output: [1, 1, 2, 3, 5, 8]
@@ -58,20 +66,26 @@ const fibLength1 = 6
 const fibLength2 = 10
 // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
-// const fib = (number)
-//   let number =  []
-//     if(number.length.indexOf[0] + number.length.indexOf[1] === + 2) {
-//       return `${number} is equal to the sum of the next two numbers`
-//     }
+const fib = (num) => {
+  const fibArray = [1, 1]
+  for (let i = 2; i <num; i++) {
+    fibArray[i] = fibArray[i -1] + fibArray[i -2]  
+  }
+    return fibArray.slice()
+}
+console.log(fib(fibLength1)) //[1, 1, 2, 3, 5, 8]
+console.log(fib(fibLength2)) //[1,  1,  2,  3,  5, 8, 13, 21, 34, 55]
 
-  // for(let i = 0; <fib.length; i++) {
-  //   if(fib[i] > 2===2) {
-  //     return `${fib[i]} + is equal to the next two numbers`
-  // } 
-  // else {fib[i] !== 2
-  //   return `${fibNum} is not equal to the sum of the next two numbers`
-  // }
-// }
+// PASSED!
+// PASS  ./code-challenges.test.js
+// fib
+//   ✓ returns an array that length containing the numbers of the Fibonacci sequence. (1 ms)
+
+// Test Suites: 1 passed, 1 total
+// Tests:       1 passed, 1 total
+// Snapshots:   0 total
+// Time:        0.129 s, estimated 1 s
+
 
 // --------------------2) Create a function that takes in an object and returns an array of the values sorted from least to greatest.
 // Hint: Check out this resource: Object.values() https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_objects/Object/values
@@ -79,33 +93,32 @@ const fibLength2 = 10
 // a) Create a test with expect statements for each of the variables provided.
 
 // Process: RED!
+
 // function takes in an object and;
 describe("myObj", () => { 
   it("returns an array of the values sorted from least to greatest.", () => {
-  // expect statement will contain the function call with a jest matcher appended to the end
-  // place test variables inside the it call back funtion
+  // expect statements will contain the function call to call the given functions and;
+   // place test variable outputs inside the .toEqual() matcher parameter to compare the given outputs
     expect(myObj(studyMinutesWeek1 )).toEqual([15, 15, 20, 30, 30, 60, 90])
     expect(myObj(studyMinutesWeek2)).toEqual([10, 15, 20, 45, 60, 65, 100])
   })
 })
-// FAIL!: "obj is not defined" 
+// FAIL!: "myObj is not defined" 
 // FAIL  ./code-challenges.test.js
-// obj
+// myObj
 //   ✕ returns an array of the values sorted from least to greatest.
 
-//     at Object.expect (code-challenges.test.js:23:5)
+// ● myObj › returns an array of the values sorted from least to greatest.
 
-// ● obj › returns an array of the values sorted from least to greatest.
-
-//   ReferenceError: obj is not defined
+// ReferenceError: myObj is not defined
 
 // b) Create the function that makes the test pass.
 
 // Process: GREEN!
 
 // PSEUDOCODE:
-// Input:2 functions with property values
-// Output: an array with values sorted from least to greatest
+// Input: 2 functions with object property values
+// Output: Array with object values sorted from least to greatest
 
 // Given outputs:
 // Input: sunday: 90, monday: 30, tuesday: 20, wednesday: 15, thursday: 30, friday: 15, saturday: 60
@@ -114,8 +127,10 @@ describe("myObj", () => {
 // Input: sunday: 100, monday: 10, tuesday: 45, wednesday: 60, thursday: 20, friday: 15, saturday: 65
 // Output: [10, 15, 20, 45, 60, 65, 100]
 
-// Create a function named "myObj" that takes in an object
-// Return an array of the values sorted from least to greatest
+// Create a function named "myObj" that takes in an object from the given functions.
+// Create a function named "values" to use the Object.values() static method to extract the object values (numbers) for each object.
+// Use the .sort() method to sort the values together with a compare function of two values (a, b) to define the sort order in ascending order.
+// Return an array of the values sorted from least to greatest.
 
 const studyMinutesWeek1 = {
   sunday: 90,
@@ -139,7 +154,22 @@ const studyMinutesWeek2 = {
 }
 // Expected output: [10, 15, 20, 45, 60, 65, 100]
 
-const myObj = {Object}
+const myObj = (object) => {
+  const values = Object.values(object)
+  return values.sort((a,b) => a-b)
+}
+console.log(myObj(studyMinutesWeek1)) //[15, 15, 20, 30,30, 60, 90]
+console.log(myObj(studyMinutesWeek2)) // [10, 15,  20, 45, 60, 65, 100]
+
+// PASSED!
+// PASS  ./code-challenges.test.js
+// myObj
+//   ✓ returns an array of the values sorted from least to greatest. (1 ms)
+
+// Test Suites: 1 passed, 1 total
+// Tests:       2 passed, 2 total
+// Snapshots:   0 total
+// Time:        0.129 s, estimated 1 s
 
 
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
@@ -147,19 +177,23 @@ const myObj = {Object}
 // a) Create a test with expect statements for each of the variables provided.
 
 // Process: RED!
-// function takes in an array;
-describe(accounting, () => { 
+
+// function takes in an array and;
+describe("accounting", () => { 
   it("returns an array of the accumulating sum. An empty array should return an empty array.", () => {
-  // expect statement will contain the function call with a jest matcher appended to the end
-  // place test variables inside the it call back funtion
+    // expect statements will contain the function call to call the given functions and;
+     // place test variable outputs inside the .toEqual() matcher parameter to compare the given outputs
     expect(accounting(accountTransactions1)).toEqual([100, 83, 60, 51])
     expect(accounting(accountTransactions2)).toEqual([250, 161, 261, 165])
     expect(accounting(accountTransactions3)).toEqual([])
   })
 })
 // // FAIL!: "accounting is not defined" 
-// FAIL  ./code-challenges.test.js
-//   ● Test suite failed to run
+//  FAIL  ./code-challenges.test.js
+// accounting
+//   ✕ returns an array of the accumulating sum. An empty array should return an empty array.
+
+// ● accounting › returns an array of the accumulating sum. An empty array should return an empty array.
 
 //   ReferenceError: accounting is not defined
 
@@ -168,8 +202,8 @@ describe(accounting, () => {
 // Process: GREEN!
 
 // PSEUDOCODE:
-// Input:2 functions with property values
-// Output: an array of the accumulating sum. An empty array should return an empty array.
+// Input: 1 array
+// Output: An array of the accumulating sum. An empty array should return an empty array.
 
 // Given outputs:
 // Input: [100, -17, -23, -9]
@@ -181,8 +215,11 @@ describe(accounting, () => {
 // Input: []
 // Output: []
 
-// Create a function named "accounting" takes in an array
-// Return an array of the accumulating sum. An empty array should return an empty array.
+// Create a function named "accounting" that takes in an array with a parameter named "arraySum".
+// Create a variable named "sum" to let the sum equal zero index(starting point).
+// Return an array of the accumulating sum by using high order function .map() method to iterate over each element in the array and keep the original array length.
+// Use a anonymous function syntax to invoke the index of zero to accumulate each number and equal the given output by taking the zero index value and using math to take the sum of the next index value to equal a new sum(new index value) and it will use the math logic for each previous index sum in the array.
+// An empty array should return an empty array.
 
 const accountTransactions1 = [100, -17, -23, -9]
 // Expected output: [100, 83, 60, 51]
@@ -193,8 +230,20 @@ const accountTransactions2 = [250, -89, 100, -96]
 const accountTransactions3 = []
 // Expected output: []
 
-const accounting = ([]) => {
-
+const accounting = (arraySum) => {
+  let sum = 0
+  return arraySum.map((value) => sum += value)
 }
+console.log(accounting(accountTransactions1)) //[100, 83, 60, 51]
+console.log(accounting(accountTransactions2)) // [250, 161, 261, 165]
+console.log(accounting(accountTransactions3)) //[]
 
+// PASSED!
+//  PASS  ./code-challenges.test.js
+// accounting
+//   ✓ returns an array of the accumulating sum. An empty array should return an empty array.
 
+// Test Suites: 1 passed, 1 total
+// Tests:       3 passed, 3 total
+// Snapshots:   0 total
+// Time:        0.168 s
--- interview-questions.md
+++ interview-questions.md
@@ -9,34 +9,38 @@ Then, research the question to expand on your answer. Even if you feel you have 
 1. What is JSX? What is one syntax difference between HTML and JSX?
 
 Your answer:
-JSX is an extension in React that allows us to use html and have the functionality of JavaScript. It can be used to add additional elements in the return area. 
+JSX is an extension in React that allows us to use HTML and have the functionality of JavaScript. It can be used to add additional elements in the return area. One syntax difference between HTML and JSX is that JSX uses a "className" syntax and HTML uses only a "class" syntax.
 
 Researched answer:
+JSX is a markdown extension of Javascript and has similiar behaviors of HTML. JSX uses the "className" syntax to modify the look and behavior of an element. JSX is a React component that allows React to create user interfaces (the user and computer interact). A syntax difference between JSX and HTML is that JSX uses "className" syntax because the "class" syntax in HTML is already protected in JavaScript.   
 
-1. What is yarn? What file(s) are modified when you run the command yarn in your terminal?
+2. What is yarn? What file(s) are modified when you run the command yarn in your terminal?
 
 Your answer:
 Yet
 Another
 Resource
 Negotiator
-Yarn is simply a code package manager. It provides the dependencies and snippets that are required to run your code.It modifies the active react file that was created in the Terminal.  
+YARN is simply a code package manager. It provides the dependencies and snippets that are required to run your code (in the git terminal input --> $ yarn add jest). Three files are modified in the active react file that was created in the terminal in order to run your code (files: yarn.lock, node_modules, package.json).  
 
 Researched answer:
+YARN is a package manager that adds all of the code snippets and dependencies for your code. It replaced the Node Package Manager (NPM), the first package manager of node.js that installed, updated and uninstalled node.js files. YARN is basically a library for web applications that runs outside of your browser.
 
 3. What is a React component?
 
 Your answer:
-A react componnent is a seperate component file that is stored under the src (source) folder in a .js (JavaScript) file. When a component is created, it allows us to organize multiple functions that can be reused over and over again. You can all on components as many times as you need. Once your funtions are complete,  components can then be imported to one main App.js file that will hold all of the state variables, values and inputs and methods. 
+A React component is a seperate component file that is stored under the src (source) folder in a .js (JavaScript) file. When a component is created, it allows us to organize multiple functions that can be reused over and over again. You can call on components as many times as you need. Once your functions are complete, components can then be imported to one main App.js file that will hold all of the state variables, values, inputs and methods. 
 
 Researched answer:
+React components are codes that we want to reuse in a function to be able to call on a function any time we want to use the code. React components are eseentially JavaScript functions. A function can contain JavaScript logic and outputs the logic that gets relayed to the browser. React components just lets us write less lines of code instead of repeating code. This is great because your main App.js file will appear less cluttered with code and because your component files will organize your code separately and you can reuse them in other applications.  
 
 4. What is the difference between state values and props in React?
 
 Your answer:
-The difference between state values and props in react are that state values stores the data that determines how a component is displayed to the user. Props is short for properties in react and is just an object that has no value. 
+The difference between state values and props in React are that state values stores the data that determines how a component is displayed to the user. Props is short for properties in React and is just an object that has no value. 
 
 Researched answer:
+The difference between state values and props in React is that state values are special variables that store data and props are special react objects that pass information from one component to another. State values define how a component is displayed and behaves. Props just display a flow of data of behavior from a parent component to a nested component. The values in state changes as the user interacts with the application. Props however cannot be changed as the user interacts with the application. We can reference variable values in state by entering a value in JSX tags using the curly braces({value}). However, we can only pass props as an argument (in a parameter - (props)) to the component then reference the variable in props as "props.name" using the JSX curly braces tag ({props.name}). 
 
 5. What is the DOM?
 
@@ -44,25 +48,45 @@ Your answer:
 Document
 Object
 Model
-Organizes the html elements like a tree and gives a visual of those html elements, 
+DOM organizes the HTML elements like a tree and gives us a visual of those HTML elements in the browser. It reads the web page in order from top to bottom (creates a tree of events/changes). Every time your state of components changes, the DOM updates only those changes.  
 
 Researched answer:
+DOM responds to user interactions. DOM makes user interactions efficient because it responds only to specific elements that were updated and changed. When you open a website you are looking at the DOM which is the visual appearance of your code.
 
 6. STRETCH: Which is the difference between a div tag and a span tag?
 
 Your answer:
-A div tag divides or separates a certain section/block of code of your choice. A div tag can be used to target certain .css stylings you want applied only to one section of code by creating a className for your div tag. For a div tag, you would have to add a dot notation followed by the className. A span tag can also be used to sepearte a certain section of code but it requires an "id" className and can only be applied to a text funtion. Also, you would have to add a hashtag symbol(#) followed by the "id" name in .css in order for your styling to be applied to the specific text of your choce. 
+A div tag divides or separates a certain section/block of code of your choice (<div></div>). A div tag can be used to target certain .css stylings you want applied only to one section/block of code by creating a "className" for your div tag. For example, for a div tag you could add a dot notation followed by the "className"(i.e. <div className=""></div>). However, a span tag (<span>text</span>) can be used to separate and target a certain section/area in your code but it requires an "id" className (<span id="idName">text</span>) and can only be applied to a text function. Also, you would have to add a hashtag symbol(#) followed by the "id" name (#idName) in a .css file in order for your styling to be applied to the specific text of your choce. 
 
 Researched answer:
+A div tag is used as a container for a block of elements in HTML and can be styled by using a class or id attribute. However a span tag is an inline element that targets only part of a text or document. Any sort of value/content can be placed inside a div tag. A div tag defines a section or division in a document or code. However, a span tag can only be used to group elements together for styling purposes (i.e. change text colors, font and font size, etc) and can use class or id attributes. A div tag wraps an entire section while a span tag highlights specific text/words (i.e. <span style="color:navy">navy</span>). 
 
 ## Looking Ahead: Terms for Next Week
 
-1. Object-oriented programming:
+1. Object-oriented programming: Object-oriented programming is a way to block off certain code that you don't want to be affected by the rest of your code when you make changes. It's a way to create data that can be changed and manipulated without affecting the application.
 
-2. Ruby:
+2. Ruby: Ruby is a scripting programming language and has many built-in methods and documentation. Ruby's style is easy to read and write. All data types are defined by class and all datatypes in Ruby are considered objects.
 
-3. Implicit return:
+3. Implicit return: Implicit return is a method in Ruby that returns a result of a final line before the end of a keyword. It is a final statement inside if the method. In order to use an implicit return in Ruby you have to use a key word "def" followed by the name of the method(use snake_case for multiple words), your code logic to be executed and the key word "end". 
 
-4. Ruby blocks:
+For example:
+def method_name
+  "Signed and sealed by Evelyn"
+end
+//output: "Signed and sealed by Evelyn"
 
-5. Ruby hashes:
+4. Ruby blocks: Ruby blocks are anonymous functions that are passed to a method which require additional information to changed the outcome. In order to use blocks you have to create your anonymous function with a value, create a method by using a dot notation after your anonymous function, use keywords "do" and "end" and be sure to enter your code logic in between the keywords for code to be executed. 
+
+For example:
+num = 2
+
+num.times do
+  "hello"
+end
+//output: "hello", "hello"
+
+5. Ruby hashes: Ruby hashes are a placeholder for unique keys and values. The structure is like an object in JavaScript but the output is a different syntax. A symbol such as a colon (:) is a unique identifier in Ruby hashes and is placed on the left side of the variable, the anonymous function symbol or "hashrocket" (=>) is followed by the variable and is used to separate the values from the keys. To use a Ruby hash, create a function name, use JSX curly braces, inside curly braces enter name followed by a colon and followed by number or string. 
+
+For example: 
+breakfast = {eggs: 2, cheese: "2 pieces", bacon: "4 slices", bread: "2 slices"}
+//output: {:eggs=>2, :cheese=> "2 pieces", bacon: "4 slices", bread: "2 slices"}
